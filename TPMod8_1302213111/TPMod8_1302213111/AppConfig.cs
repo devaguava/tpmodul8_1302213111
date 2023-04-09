cc9df7d05fc915ef55d76c5b627e3ad81db752c2
devaguava <117663776+devaguava@users.noreply.github.com>
2023-04-09 13:46:57 +0700

    menyelesaikan program dengan runtime configuration

---
+++ TPMod8_1302213111/TPMod8_1302213111/AppConfig.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text.Json;
+
+public class AppConfig
+{
+	public CovidConfig covid_convig;
+
+	public const string filePath = @"covid_config.json";
+
+	public AppConfig()
+	{
+		try
+		{
+			ReadConfigFile();
+		}
+		catch 
+		{
+			SetDefault();
+			WriteNewConfigFile();
+		}
+	}
+
+	private CovidConfig ReadConfigFile()
+	{
+		string configJsonData = File.ReadAllText(filePath);
+		covid_convig = JsonSerializer.Deserialize<CovidConfig>(configJsonData);
+		return covid_convig;
+	}
+
+	private void SetDefault()
+	{
+		string pesanDitolak = "Anda tidak diperbolehkan masuk ke dalam gedung ini";
+		string pesanDiterima = "Anda dipersilahkan untuk masuk ke dalam gedung ini";
+		covid_convig = new CovidConfig("celcius", 14, pesanDitolak, pesanDiterima);
+	}
+
+    private void WriteNewConfigFile()
+    {
+        JsonSerializerOptions options = new JsonSerializerOptions
+        {
+            WriteIndented = true
+        };
+
+        string jsonString = JsonSerializer.Serialize(covid_convig, options);
+        File.WriteAllText(filePath, jsonString);
+    }
+}
--- TPMod8_1302213111/TPMod8_1302213111/Program.cs
+++ TPMod8_1302213111/TPMod8_1302213111/Program.cs
@@ -1,2 +1,50 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        AppConfig config = new AppConfig();
+        config.covid_convig.UbahSatuan();
+
+        Console.Write($"Berapa suhu badan Anda saat ini? Dalam nilai {config.covid_convig.satuan_suhu} : ");
+        double suhu = double.Parse(Console.ReadLine());
+
+        Console.Write("Berapa hari yang lalu (perkiraan) Anda terakhir memiliki gejala demam? ");
+        int hari = int.Parse(Console.ReadLine());
+
+        try
+        {
+            if(hari < 0)
+            {
+                throw new Exception("Invalid");
+            }
+            if(config.covid_convig.satuan_suhu == "celcius")
+            {
+                if(suhu >= 36.5 && suhu <= 37.5 && hari < config.covid_convig.batas_hari_demam)
+                {
+                    Console.WriteLine(config.covid_convig.pesan_diterima);
+                }
+                else
+                {
+                    Console.WriteLine(config.covid_convig.pesan_ditolak);
+                }
+            }
+            else
+            {
+                if (suhu >= 97.7 && suhu <= 99.5 && hari < config.covid_convig.batas_hari_demam)
+                {
+                    Console.WriteLine(config.covid_convig.pesan_diterima);
+                }
+                else
+                {
+                    Console.WriteLine(config.covid_convig.pesan_ditolak);
+                }
+            }
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
